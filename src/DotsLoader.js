39983970ae414f409ca1508d51bba48fe9b9e298
fasihh <fhk.tcs@gmail.com>
2024-02-04 23:10:01 +0500

    added dots loader

---
+++ src/DotsLoader.js
@@ -0,0 +1,12 @@
+const DotsLoader = () => {
+    return ( 
+        <div className="bouncing-dots">
+            <p className="message">Loading posts </p>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+     );
+}
+ 
+export default DotsLoader;
